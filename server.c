0180cd899093dc26cc1821b844eb7d21f5e7aae2
Guppi4 <kirill.perevalov.1996@gmail.com>
2022-04-15 00:45:08 +0300

    final

--- server.c
+++ server.c
@@ -1,103 +1,168 @@
-#include <sys/socket.h>
 
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <pthread.h>//treds
+
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <pthread.h>
+#define PORT "3456"  // the port users will be connecting to
 
-void *tr_handler(void *socket_desc)
+#define BACKLOG 10   // how many pending connections queue will hold
+
+char client_message[2000];
+char buffer[1024];
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+void * socketThread(void *arg)
 {
-    
-    int sock = *(int *)socket_desc;
-    int read_size;
-    char *message, client_message[30000];
+  ///printf("%s","hgjh");
+  int newSocket = *((int *)arg);
+  recv(newSocket , client_message , 2000 , 0);
 
-   
-    message = "Greetings!\n"; // send  messages to the client
-    write(sock, message, strlen(message));
+  // Send message to the client socket 
+  pthread_mutex_lock(&lock);
+  char *message = malloc(sizeof(client_message)+20);
+  strcpy(message,"Hello Client : ");
+  strcat(message,client_message);
+  strcat(message,"\n");
+  strcpy(buffer,"Hello Client : ");
+  free(message);
+  pthread_mutex_unlock(&lock);
+  sleep(1);
+  printf("%s",buffer);
+  send(newSocket,buffer,13,0);
+  printf("Exit socketThread \n");
+  close(newSocket);
+  pthread_exit(NULL);
+}
+void sigchld_handler(int s)
+{
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
 
-   
+    while(waitpid(-1, NULL, WNOHANG) > 0);
 
-    
-    while ((read_size = recv(sock, client_message, 2000, 0)) > 0)//a message from client
-    {
-       
-        write(sock, client_message, strlen(client_message));
-    }
-
-    if (read_size == 0)
-    {
-        puts("Client_disconnected");
-        fflush(stdout);
-    }
-    else if (read_size == -1)
-    {
-        perror("recv_failed");
-    }
-
-    
-    free(socket_desc);
-
-    return 0;
+    errno = saved_errno;
 }
 
-int main(int argc, char *argv[])
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
 {
-    int socket_desc, client_sock, size, *new_sock;
-    struct sockaddr_in server, client;
-
-    socket_desc = socket(AF_INET, SOCK_STREAM, 0); // Create socket
-    if (socket_desc == -1)
-    {
-        printf("not create socket");
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
     }
-    puts("socket created");
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(8776);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
-    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) // bind
-    {
+int main(void)
+{
+    int sockfd, new_fd,*new_sock;  // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes=1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
 
-        perror("bind failed. Error");
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
-    puts("bind done");
 
-  
-    listen(socket_desc, 3);  
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("server: socket");
+            continue;
+        }
 
-    size= sizeof(struct sockaddr_in);
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
+                sizeof(int)) == -1) {
+            perror("setsockopt");
+            exit(1);
+        }
 
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if (p == NULL)  {
+        fprintf(stderr, "server: failed to bind\n");
+        exit(1);
+    }
+
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen");
+        exit(1);
+    }
+
+    sa.sa_handler = sigchld_handler; // reap all dead processes
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(1);
+    }
+    pthread_t tid[60];
+    int i = 0;
+    printf("server: waiting for connections...\n");
+
+    while(1) {  // main accept() loop
+        sin_size = sizeof their_addr;
+        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        if (new_fd == -1) {
+            perror("accept");
+            continue;
+        }
     
-    puts("Waiting for  connections...");// Accept and incoming connection
-    size= sizeof(struct sockaddr_in);
-    while ((client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&size)))
-    {
+        inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            s, sizeof s);
+        //printf("server: got connection from %s\n", s);
         puts("accepted");
 
-        pthread_t sniffer_thread;
-        new_sock = malloc(1);
-        *new_sock = client_sock;
-
-        if (pthread_create(&sniffer_thread, NULL, tr_handler, (void *)new_sock) < 0)
+         if( pthread_create(&tid[i++], NULL,&socketThread, &new_fd) != 0 ){
+           printf("Failed to create thread\n");
+           }
+       
+       if( i >= 50)
         {
-            perror("not create thread");
-            return 1;
+          i = 0;
+          while(i < 50)
+          {
+            pthread_join(tid[i++],NULL);
+          }
+          i = 0;
         }
-            puts("handler into server");
-    }   
-    if (client_sock < 0)
-    {
-        perror("failed");
-        return 1;
-    }
     
+    }   
+    
+
+
     return 0;
 }
-
-
-
